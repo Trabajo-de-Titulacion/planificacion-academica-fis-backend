554af07043759cf9f8647e09a8a9a6b9f7b01704
Alejandro Llanganate <luis.llanganate@epn.edu.ec>
2022-06-23 14:03:21 -0400

    refactor(users): add users module
    Separate users from auth module.
    
    @alejandrollanganate - EPN

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { User } from './entities/user.entity';
+import { UserDto } from '../users/dtos/user.dto';
+import { User } from '../users/entities/user.entity';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(User) private userRepository : Repository<User>
     ){}
+
+    signInLocal(user  : UserDto){
+
+    }
+
+    signUpLocal(user  : UserDto){
+
+    }
 }
