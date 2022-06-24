b21ebb44a2b9c5e7e83503430decc12db9ba44a5
Alejandro Llanganate <luis.llanganate@epn.edu.ec>
2022-06-24 19:56:53 -0400

    fix: postgres db connection (#1)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot(
+      {
+        isGlobal: true,
+      }
+    ),
+    AuthModule,
+    UsersModule,
+    TypeOrmModule.forRoot(
+      {
+        type: 'postgres',
+        host: process.env.POSTGRES_HOST,
+        port: Number(process.env.POSTGRES_PORT),
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
+        autoLoadEntities: true,
+        synchronize: true,
+      }
+    )
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
