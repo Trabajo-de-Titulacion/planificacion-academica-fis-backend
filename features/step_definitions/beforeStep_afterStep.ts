735c3be478c4ed4c0f7c6a9056be7cf747f693fb
Mahatma Quijano <mahatma.quijano@epn.edu.ec>
2022-07-19 18:54:29 -0500

    feat: beforeStep afterStep
    Se añade el beforeStep y afterStep para iniciar la aplicación antes de cada step

---
+++ features/step_definitions/beforeStep_afterStep.ts
@@ -0,0 +1,24 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../../src/app.module';
+
+import { AfterStep, BeforeStep } from '@cucumber/cucumber';
+
+BeforeStep(async function () {
+  // Levantar aplicacion
+  const app = await NestFactory.create(AppModule, {
+    logger: false,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  await app.listen(3000);
+  this.app = await app;
+});
+
+AfterStep(async function () {
+  // Cerrar aplicacion
+  this.app.close();
+});
