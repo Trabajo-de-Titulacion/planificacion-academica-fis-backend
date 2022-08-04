d7c59455d730cd4c0d340c47925be2c8777d0625
Mahatma Quijano <mahatma.quijano@epn.edu.ec>
2022-08-03 19:31:47 -0500

    feat: CORS

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import { configuracionesSwagger } from './config/swagger-config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+  
   // Configuraciones para Swagger
   const configuracionSwagger = new DocumentBuilder()
     .setTitle(configuracionesSwagger.titulo)
