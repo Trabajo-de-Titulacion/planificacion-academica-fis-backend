9fbb09819ccf0aaf245b51f325a2c5389ca72f9f
Mahatma Quijano <mahatma.quijano@epn.edu.ec>
2022-08-14 18:31:33 -0500

    feat(Horas no disponibles): Guardas para verificar docente
    Se implementaron guardas para verificar que un docente solo puede visualizar o modificar la información que le pertenezca a sí mismo.

--- src/horas_no_disponibles/horas_no_disponibles.module.ts
+++ src/horas_no_disponibles/horas_no_disponibles.module.ts
@@ -10,11 +10,13 @@ import { JornadaLaboralService } from '../../src/parametros-iniciales/services/j
 import { JornadaLaboralEntity } from '../../src/parametros-iniciales/entities/jornada-laboral.entity';
 import { SemestreService } from '../../src/parametros-iniciales/services/semestre.service';
 import { SemestreEntity } from '../../src/parametros-iniciales/entities/semestre.entity';
+import { AuthModule } from '../../src/auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([HoraNoDisponibleEntity, DocenteEntity, JornadaLaboralEntity, SemestreEntity]),
-    DocenteModule
+    DocenteModule, AuthModule, JwtModule,
   ],
   controllers: [HorasNoDisponiblesController],
   providers: [HorasNoDisponiblesService, DocenteService, JornadaLaboralService, SemestreService],
