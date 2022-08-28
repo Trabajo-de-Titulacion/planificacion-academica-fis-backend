f0b73e0e17b8827833ee25f46e5c63ab0b09e29c
Diana <diana.lopez03@epn.edu.ec>
2022-08-28 15:24:55 -0500

    Add(carrera): Creación de los pasos para pasar las pruebas

--- features/step_definitions/registro_de_carreras.ts
+++ features/step_definitions/registro_de_carreras.ts
@@ -1,5 +1,5 @@
 const assert = require('assert');
-import { Given, When, Then } from '@cucumber/cucumber';
+import { Given, When, Then, After } from '@cucumber/cucumber';
 import { CarreraController } from '../../src/carrera/controllers/carrera.controller';
 import { CarreraEntity } from '../../src/carrera/entities/carrera.entity';
 import { getRepository } from 'typeorm';
@@ -7,61 +7,67 @@ import { getRepository } from 'typeorm';
 
 /* ESCENARIO 1 - Ingreso individual de un carrera */
 
-Given('que se tiene una carrera con código {string}, nombre {string}, duración {string} y modalidad {string}', async function (codigo_carrera_existente, nombre_carrera_existente, duracion_carrera_existente, modalidad_carrera_existente) {
+Given('que se tiene una carrera con código {string}, nombre {string}, duración {string} y modalidad {string}', { timeout: 5 * 5000 }, async function (codigoCarreraExistente, nombreCarreraExistente, duracionCarreraExistente, modalidadCarreraExistente) {
     /*
     Los datos recibidos son:
-    |   Significado                     | Nombre variable             | Tipo de dato necesario |
-    |Código de la carrera               | codigo_carrera_existente    | string                 |
-    |Nombre de la carrera               | nombre_carrera_existente    | string                 |
-    |Duración de la carrera (semestres) | duracion_carrera_existente  | number                 |
-    |Modalidad de la carrrera           | modalidad_carrera_existente | string                 |
+    |   Significado                     | Nombre variable           | Tipo de dato necesario |
+    |Código de la carrera               | codigoCarreraExistente    | string                 |
+    |Nombre de la carrera               | nombreCarreraExistente    | string                 |
+    |Duración de la carrera (semestres) | duracionCarreraExistente  | number                 |
+    |Modalidad de la carrrera           | modalidadCarreraExistente | string                 |
     */
-
-    // Convertir a tipos de datos correspondientes
-
-    duracion_carrera_existente = Number(duracion_carrera_existente);
 
     // Declarar e ingresar la carrera previa para comprobación
 
-    await getRepository(CarreraEntity).save({
-        codigo: codigo_carrera_existente,
-        nombre: nombre_carrera_existente,
-        duracion: duracion_carrera_existente,
-        modalidad: modalidad_carrera_existente
-    })
+    this.carreraExistente = {
+        codigo: codigoCarreraExistente,
+        nombre: nombreCarreraExistente,
+        duracion: Number(duracionCarreraExistente),
+        modalidad: modalidadCarreraExistente
+    }
+    await getRepository(CarreraEntity).save(this.carreraExistente);
 });
 
-When('se ingrese una carrera con código {string}, nombre {string}, duración {string} y modalidad {string}', async function (codigo_carrera_nueva, nombre_carrera_nueva, duracion_carrera_nueva, modalidad_carrera_nueva) {
+When('se ingrese una carrera con código {string}, nombre {string}, duración {string} y modalidad {string}', { timeout: 5 * 5000 }, async function (codigoCarreraNueva, nombreCarreraNueva, duracionCarreraNueva, modalidadCarreraNueva) {
     /*
     Los datos recibidos son:
-    |   Significado                     | Nombre variable         | Tipo de dato necesario |
-    |Código de la carrera               | codigo_carrera_nueva    | string                 |
-    |Nombre de la carrera               | nombre_carrera_nueva    | string                 |
-    |Duración de la carrera (semestres) | duracion_carrera_nueva  | number                 |
-    |Modalidad de la carrrera           | modalidad_carrera_nueva | string                 |
+    |   Significado                     | Nombre variable       | Tipo de dato necesario |
+    |Código de la carrera               | codigoCarreraNueva    | string                 |
+    |Nombre de la carrera               | nombreCarreraNueva    | string                 |
+    |Duración de la carrera (semestres) | duracionCarreraNueva  | number                 |
+    |Modalidad de la carrrera           | modalidadCarreraNueva | string                 |
     */
 
     // Ingresar carrera nueva
 
     this.carreraController = await this.app.get(CarreraController);
-    this.resultado_ingresar_carrera = await this.carreraController.createCarrera({
-        codigo: codigo_carrera_nueva,
-        nombre: nombre_carrera_nueva,
-        duracion: duracion_carrera_nueva,
-        modalidad: modalidad_carrera_nueva
-    })
+
+    this.carreraNueva = {
+        codigo: codigoCarreraNueva,
+        nombre: nombreCarreraNueva,
+        duracion: Number(duracionCarreraNueva),
+        modalidad: modalidadCarreraNueva
+    }
+
+    this.resultadoIngresarCarrera = await this.carreraController.crearUnaCarrera(this.carreraNueva);
 
 });
 
-Then('se obtendrá la respuesta {string} del sistema', async function (respuesta_ingreso_carrera) {
+Then('se obtendrá la respuesta {string}.', { timeout: 5 * 5000 }, async function (respuestaIngresoCarrera) {
     /*
     Los datos recibidos son: 
-    |           Significado           | Nombre variable              | Tipo de dato necesario |
-    | Cantidad de carreras ingresadas | cantidad_carreras_ingresados | string                 |
+    |           Significado           | Nombre variable            | Tipo de dato necesario |
+    | Cantidad de carreras ingresadas | cantidadCarrerasIngresados | string                 |
      */
 
     // Realizar comprobación
 
-    assert.equal(this.resultado_ingresar_carrera, respuesta_ingreso_carrera);
+    assert.equal(this.resultadoIngresarCarrera.mensaje, respuestaIngresoCarrera);
 });
 
+After("@RegistroDeCarrera", async function () {
+
+    // Eliminar carreras usadas
+    await getRepository(CarreraEntity).delete(this.carreraExistente);
+    await getRepository(CarreraEntity).delete(this.carreraNueva);
+});
