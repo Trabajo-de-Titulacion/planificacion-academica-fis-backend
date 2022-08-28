1bc7006a58efd047bf2331e720ebd0bcb771f759
Diana <diana.lopez03@epn.edu.ec>
2022-08-28 15:26:31 -0500

    Add(carreras): Creación de los métodos del controlador y servicio

--- src/carrera/controllers/carrera.controller.ts
+++ src/carrera/controllers/carrera.controller.ts
@@ -1,21 +1,90 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { CarreraService } from '../services/carrera.service';
-import { CarreraDto } from '../dto/carrera.dto';
+import { CarreraDto } from '../../carrera/dto/carrera.dto';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { configuraciones } from '../../config/swagger-config';
+import { Roles } from '../../auth/decorators/roles.decorator';
+import { RolesEnum } from '../../utils/enum/rol.enum';
 
-@Controller('carrera')
+@ApiBearerAuth('defaultBearerAuth')
+@ApiTags(configuraciones.controladores.carrera.tag)
+@Controller(configuraciones.controladores.carrera.ruta)
 export class CarreraController {
 
     constructor(private carreraService: CarreraService) { }
 
-    @Post('crearUno')
-    createCarrera(@Body() carreraDto: CarreraDto) {
+    /* =================================================================================================================== */
+    /* ======================================= CREAR UNA CARRERA EN LA BASE DE DATOS ===================================== */
+    /* =================================================================================================================== */
 
-        // Convertir en mayúsculas el código, nombre y modalidad
-
+    @ApiOperation({ summary: configuraciones.controladores.carrera.operaciones.crearUnaCarrera.descripcion })
+    @Post(configuraciones.controladores.carrera.operaciones.crearUnaCarrera.ruta)
+    @Roles(RolesEnum.COORDINADOR)
+    crearUnaCarrera(@Body() carreraDto: CarreraDto) {
+        // Formatear y convertir en mayúsculas
         carreraDto.codigo = carreraDto.codigo.toUpperCase();
         carreraDto.modalidad = carreraDto.modalidad.toUpperCase();
         carreraDto.nombre = carreraDto.nombre.toUpperCase();
-
-        return this.carreraService.createCarrera(carreraDto);
+        return this.carreraService.crearUnaCarrera(carreraDto);
     }
+
+    /* =================================================================================================================== */
+    /* =================================== ACTUALIZAR UNA CARRERA EN LA BASE DE DATOS ==================================== */
+    /* =================================================================================================================== */
+
+    @ApiOperation({ summary: configuraciones.controladores.carrera.operaciones.actualizarCarreraPorID.descripcion })
+    @Put(configuraciones.controladores.carrera.operaciones.actualizarCarreraPorID.ruta)
+    @Roles(RolesEnum.COORDINADOR)
+    actualizarCarreraPorID(@Param('id') idCarrera: string, @Body() carreraDto: CarreraDto) {
+        // Formatear y convertir en mayúsculas 
+        carreraDto.codigo = carreraDto.codigo.toUpperCase();
+        carreraDto.modalidad = carreraDto.modalidad.toUpperCase();
+        carreraDto.nombre = carreraDto.nombre.toUpperCase();
+        return this.carreraService.actualizarCarreraPorID(idCarrera, carreraDto);
+    }
+
+    /* =================================================================================================================== */
+    /* ==================================== ELIMINAR UNA CARRERA EN LA BASE DE DATOS ===================================== */
+    /* =================================================================================================================== */
+
+    @ApiOperation({ summary: configuraciones.controladores.carrera.operaciones.eliminarCarreraPorID.descripcion })
+    @Delete(configuraciones.controladores.carrera.operaciones.eliminarCarreraPorID.ruta)
+    @Roles(RolesEnum.COORDINADOR)
+    eliminarCarreraPorID(@Param('id') idCarrera: string) {
+        return this.carreraService.eliminarCarreraPorID(idCarrera);
+    }
+
+    /* ====================================================================================================================== */
+    /* ============================= OBTENER UNA CARRERA POR SU CÓDIGO EN LA BASE DE DATOS ================================== */
+    /* ====================================================================================================================== */
+
+    @ApiOperation({ summary: configuraciones.controladores.carrera.operaciones.obtenerCarreraPorCodigo.desripcion })
+    @Get(configuraciones.controladores.carrera.operaciones.obtenerCarreraPorCodigo.ruta)
+    @Roles(RolesEnum.COORDINADOR)
+    obtenerCarreraPorCodigo(@Param('codigo') codigoCarrera: string) {
+        return this.carreraService.obtenerCarreraPorCodigo(codigoCarrera);
+    }
+
+    /* ====================================================================================================================== */
+    /* =============================== OBTENER UNA CARRERA POR SU ID EN LA BASE DE DATOS ==================================== */
+    /* ====================================================================================================================== */
+
+    @ApiOperation({ summary: configuraciones.controladores.carrera.operaciones.obtenerCarreraPorID.descripcion })
+    @Get(configuraciones.controladores.carrera.operaciones.obtenerCarreraPorID.ruta)
+    @Roles(RolesEnum.COORDINADOR)
+    obtenerCarreraPorID(@Param('id') idCarrera: string) {
+        return this.carreraService.obtenerCarreraPorID(idCarrera);
+    }
+
+    /* ====================================================================================================================== */
+    /* ================================= OBTENER TODAS LAS CARRERA EN LA BASE DE DATOS ====================================== */
+    /* ====================================================================================================================== */
+
+    @ApiOperation({ summary: configuraciones.controladores.carrera.operaciones.obtenerCarreras.descripcion })
+    @Get(configuraciones.controladores.carrera.operaciones.obtenerCarreras.ruta)
+    @Roles(RolesEnum.COORDINADOR)
+    obtenerCarreras() {
+        return this.carreraService.obtenerCarreras();
+    }
+
 }
